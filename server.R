6b1dc7301a673e42425c2fb48170f363ab5abc3d
Brianna Heggeseth <bcheggeseth@gmail.com>
2018-12-10 09:07:42 -0600

    Working version

--- server.R
+++ server.R
@@ -1,13 +1,12 @@
 library(shiny)
 library(DT)
-library(tidyr)
 library(dplyr)
+library(tidyr)
 library(ggmosaic)
 library(markdown)
 library(viridis)
 
 df <- read.csv('artistdata.csv')
-
 
 levels(df$gender) = c('Man','Woman')
 df$gender = as.character(df$gender)
@@ -37,46 +36,39 @@ levels(df$nationality) = c("Africa", "Asia/Pacific", "Europe", "Latin America/Ca
 df$nationality = factor(df$nationality, levels = c("Africa", "Asia/Pacific","West Asia", "Latin America/Caribbean","Europe","North America","Not Inferred"))
 
 MLevels = levels(df$museum)
-
-#WMAA, MOMA, MOCA, SFMOMA, DIA, NGA,MFAB, MMA, PMA, AIC, NAMA< RISDM, YUAG, LACMA, HMA, DAM, DMA, MFAH
-#MOCA, WMAA, DAM, MOMA, LACMA, MFAB, RISDM, YUAG, HMA, dia, gna, nama, mma, pma, aic, sfmoma, dma, mfah
-source('scratchpaper.R')
+df$museum = factor(df$museum,levels = MLevels[c(4,7,9,12,14,1,5,13,15,18,2,3,6,10,8,11,16,17)])
 
 
 shinyServer(function(input, output) {
   observe({
-  filters = ifelse(input$filter,'top','none')
-  
-  output$artistdata <- renderDT(
-    df[,c('museum','artist','gender','ethnicity','nationality','birthyear')] %>% arrange(museum,artist), 
-    colnames = c( 'Museum','Artist', 'Gender', 'Ethnicity', 'Regional Origin', 'Birth Year'),
-    rownames = FALSE,
-    extensions = 'FixedHeader',
-    filter = filters,
-    options = list(fixedHeader = TRUE)
-  )
-})
-  
+    filters = ifelse(input$filter,'top','none')
     
+    output$artistdata <- renderDT(
+      df[,c('museum','artist','gender','ethnicity','nationality','birthyear')] %>% arrange(museum,artist), 
+      colnames = c( 'Museum','Artist', 'Gender', 'Ethnicity', 'Regional Origin', 'Birth Year'),
+      rownames = FALSE,
+      extensions = 'FixedHeader',
+      filter = filters,
+      options = list(fixedHeader = TRUE)
+    )
+  })
+  
+  
   output$demoplot <- renderPlot({
     dftmp = df
     if(input$unknownfilter){
-      eval(parse(text = paste0("dftmp = dftmp %>% dplyr::filter(",input$demovar,"!= 'Not Inferred') %>% droplevels()")))
-   }
-    if(input$diversity){
-      dftmp$museum = factor(dftmp$museum,levels = MLevels[divOrd])
-    }else{dftmp$museum = factor(dftmp$museum,levels = MLevels[colOrd])}
-    
+      dftmp = dftmp %>% filter_(paste0(input$demovar,"!= 'Not Inferred'")) %>% droplevels()
+    }
     Levels = levels(eval(parse(text=paste0('dftmp$',input$demovar))))
     barplot <- ggplot(data=dftmp, aes_string(x = input$demovar)) + 
       geom_bar(aes(y = ..prop.., fill = factor(..x..), group = 1)) + facet_wrap(~museum, ncol=3) +
       scale_fill_viridis_d(name=tools::toTitleCase(input$demovar),
-                          breaks=1:length(Levels),
-                          labels=Levels) +
+        breaks=1:length(Levels),
+        labels=Levels) +
       xlab(tools::toTitleCase(input$demovar)) +
       ylab('Proportion') +
       coord_flip() +
-      theme_classic(base_size = 16) +
+      theme_classic(base_size = 18) +
       theme(axis.text.x = element_text(angle = 0, hjust = 1),axis.text.y = element_text(angle = 0, hjust = 1)) + 
       theme(legend.position="top")
     
@@ -86,11 +78,11 @@ shinyServer(function(input, output) {
       xlab('Museum') +
       ylab('Proportion') +
       coord_flip() +
-      theme_classic(base_size = 16) +
+      theme_classic(base_size = 18) +
       theme(axis.text.x = element_text(angle = 0, hjust = 1),axis.text.y = element_text(angle = 0, hjust = 1)) +
       theme(legend.position="top") 
-      
-      
+    
+    
     ifelse(input$barplot,return(barplot),return(mosaicplot))
   })
   
@@ -102,7 +94,7 @@ shinyServer(function(input, output) {
   #    Unknown=sum(is.na(gender))/n()
   #  )
   #  museumdata.long <-  gather(museumdata, "Gender", "Proportion", c("Male","Female","Unknown"), factor_key=TRUE) 
-    # %>% group_by(museum) %>% arrange(Proportion, .by_group=TRUE)
+  # %>% group_by(museum) %>% arrange(Proportion, .by_group=TRUE)
   #  plot <- ggplot(museumdata.long, aes(x=Gender, y=Proportion, fill=Gender)) +
   #    geom_bar(stat="identity", width=0.7) + facet_wrap(~museum, ncol=3) +
   #    guides(fill = guide_legend(title="Gender:")) + 
